333b6433c42456e28bf4e7ca8e4bc67ca6a9a6d5
Satish K C <kcsatish53@gmail.com>
2026-04-01 18:51:12 -0500

    fix: move FrozenTrial import into TYPE_CHECKING block in storages/_callbacks.py

--- optuna/storages/_callbacks.py
+++ optuna/storages/_callbacks.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
 from typing import Any
 
 import optuna
 from optuna._experimental import experimental_class
 from optuna._experimental import experimental_func
-from optuna.trial import FrozenTrial
+
+if TYPE_CHECKING:
+    from optuna.trial import FrozenTrial
 
 
 @experimental_class("2.8.0")
