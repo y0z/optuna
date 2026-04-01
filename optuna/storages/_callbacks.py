e30b2684446b0aa2846434ead124c459039d8395
Satish K C <kcsatish53@gmail.com>
2026-04-01 18:59:00 -0500

    fix: sort TYPE_CHECKING before Any in import block

--- optuna/storages/_callbacks.py
+++ optuna/storages/_callbacks.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
 from typing import Any
+from typing import TYPE_CHECKING
 
 import optuna
 from optuna._experimental import experimental_class
 from optuna._experimental import experimental_func
+
 
 if TYPE_CHECKING:
     from optuna.trial import FrozenTrial
